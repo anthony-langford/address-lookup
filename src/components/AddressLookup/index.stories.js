2b52a0a4a601597666464356c3d6d6fb9328aa3a
Anthony Langford <anthony.langford@gmail.com>
2019-12-10 16:24:38 -0500

    setup storybook 📘

---
+++ src/components/AddressLookup/index.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// Components
+import AddressLookup from "./AddressLookup";
+
+export default { title: "Address Lookup" };
+
+export const withText = () => <AddressLookup />;
