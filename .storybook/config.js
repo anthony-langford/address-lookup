e92e771fa76b4818f3a295e4639d4d5b7981ffcc
Anthony Langford <anthony.langford@gmail.com>
2019-12-11 16:55:25 -0500

    config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from '@storybook/react';
 import 'antd/dist/antd.css';
 import '../src/styles/index.scss';
+import './styles.scss';
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
