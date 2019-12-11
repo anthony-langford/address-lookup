2cc9841ef4339a153d021c1754447388050de8a5
Anthony Langford <anthony.langford@gmail.com>
2019-12-11 12:32:08 -0500

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,48 @@
-var path = require("path");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2"
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[chunkhash].js',
+    libraryTarget: 'commonjs2',
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
+        include: path.resolve(__dirname, 'src'),
         exclude: /(node_modules|bower_components|build)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          // MiniCssExtractPlugin,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
   externals: {
-    react: "commonjs react"
-  }
+    react: 'commonjs react',
+  },
 };
